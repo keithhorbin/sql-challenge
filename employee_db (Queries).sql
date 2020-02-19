b5718101f71888249209a43c14c6e7c71eddb4e8
Keith Horbin <keith.horbin.ds@gmail.com>
2020-02-18 19:14:46 -0500

    SQL tweaks

--- employee_db (Queries).sql
+++ employee_db (Queries).sql
@@ -21,6 +21,8 @@ SELECT employees.emp_no, employees.last_name, employees.first_name, departments.
 FROM employees
 JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_emp.dept_no
+--Including code as markdown to pull only employees in their current position
+--WHERE EXTRACT(YEAR FROM dept_emp.to_date)= '9999'
 ORDER BY emp_no;
 
 --5.List all employees whose first name is "Hercules" and last names begin with "B."
@@ -32,18 +34,24 @@ SELECT employees.emp_no, employees.last_name, employees.first_name, departments.
 FROM employees
 JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_emp.dept_no
-WHERE departments.dept_name = 'Sales';
+WHERE
+--Including code as markdown to pull only employees currently in a Sales position
+--EXTRACT(YEAR FROM dept_emp.to_date)= '9999' AND
+departments.dept_name = 'Sales';
 
 --7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
 JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_emp.dept_no
-WHERE (departments.dept_name = 'Sales' OR departments.dept_name = 'Development') 
+WHERE 
+--Including code as markdown to pull only employees currently in a Development or Sales position
+--EXTRACT(YEAR FROM dept_emp.to_date)= '9999' AND
+departments.dept_name = 'Sales' OR departments.dept_name = 'Development' 
 ORDER BY departments.dept_name ASC, emp_no ASC;
 
 --8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, COUNT(last_name) AS TotalCount
 FROM employees
 GROUP BY last_name
-ORDER BY TotalCount DESC;
+ORDER BY TotalCount DESC, last_name ASC;
---
+++ pg_key.py
@@ -0,0 +1,4 @@
+# Postgres user and password
+my_pg_user = "postgres"
+
+my_pg_key = "postgres"
